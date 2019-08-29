3f9b8b1f0c9977a9888784e2cd682b1aa31fffa9
unknown <aguirreh8@gmail.com>
2019-08-28 23:14:51 -0400

    Create delete button to new note card

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,16 @@ submitBtn.addEventListener("click", function(e) {
         const newNoteCard = document.createElement("div");
         const newNoteTitle = document.createElement("h3");
         const newNoteMessage = document.createElement("p");
+        const newNoteDltBtn = document.createElement("button");
+        newNoteDltBtn.innerHTML = "X";
 
         noteTitle ? newNoteTitle.innerHTML = noteTitle : newNoteTitle.innerHTML = "No Title";
         newNoteMessage.innerHTML = noteMessage;
         
         newNoteCard.classList.add("note-card");
+        newNoteDltBtn.classList.add("delete-note-btn");
+        
+        newNoteCard.appendChild(newNoteDltBtn);
         newNoteCard.appendChild(newNoteTitle);
         newNoteCard.appendChild(newNoteMessage);
         notesHolder.appendChild(newNoteCard);
