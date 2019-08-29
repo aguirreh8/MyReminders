a5085d02b742b498e880a4af0b2e3bcbf179b22d
unknown <aguirreh8@gmail.com>
2019-08-28 22:34:44 -0400

    Create new element from javascript, insert values from input bars into new elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,23 @@ const notesHolder = document.getElementById("notesHolder");
 
 submitBtn.addEventListener("click", function(e) {
     e.preventDefault();
-    // console.log("Hello world!");
+    
+    const noteTitle = document.getElementById("noteTitle").value;
     const noteMessage = document.getElementById("noteMessage").value;
-    if (!noteMessage) {
-        alert("Please type in a  message");
-    } else {
+    if (noteMessage) {
         const newNoteCard = document.createElement("div");
+        const newNoteTitle = document.createElement("h3");
+        const newNoteMessage = document.createElement("p");
+
+        noteTitle ? newNoteTitle.innerHTML = noteTitle : newNoteTitle.innerHTML = "No Title";
+        newNoteMessage.innerHTML = noteMessage;
+        
         newNoteCard.classList.add("note-card");
+        newNoteCard.appendChild(newNoteTitle);
+        newNoteCard.appendChild(newNoteMessage);
         notesHolder.appendChild(newNoteCard);
+    } else {
+        alert("Please type in a message");
     }
 })
 
