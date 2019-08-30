2fc101593ed85546a1395dad6a3b4e70d86fdec4
unknown <aguirreh8@gmail.com>
2019-08-29 23:05:06 -0400

    Create function to get id from dynamically created element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 const submitBtn = document.getElementById("submitBtn");
 const notesHolder = document.getElementById("notesHolder");
+const clearBtn = document.getElementById("clearBtn");
 
 submitBtn.addEventListener("click", function(e) {
     e.preventDefault();
     
     const noteTitle = document.getElementById("noteTitle").value;
     const noteMessage = document.getElementById("noteMessage").value;
+    let generateId = String.fromCharCode(65 + Math.floor(Math.random() * 26)) + 
+        Date.now();
+    
     if (noteMessage) {
         const newNoteCard = document.createElement("div");
         const newNoteTitle = document.createElement("h3");
@@ -17,7 +21,10 @@ submitBtn.addEventListener("click", function(e) {
         newNoteMessage.innerHTML = noteMessage;
         
         newNoteCard.classList.add("note-card");
+        newNoteCard.setAttribute("id", generateId);
         newNoteDltBtn.classList.add("delete-note-btn");
+        newNoteDltBtn.setAttribute("id", generateId);
+        newNoteDltBtn.setAttribute("onclick", "deleteNote(this.id)")
         
         newNoteCard.appendChild(newNoteDltBtn);
         newNoteCard.appendChild(newNoteTitle);
@@ -28,3 +35,12 @@ submitBtn.addEventListener("click", function(e) {
     }
 })
 
+clearBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+})
+
+
+
+const deleteNote = (id) => {
+    console.log(id);
+}
